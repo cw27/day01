cc1ff6955c03e29d8fbb00c4d0f44baa3f1200b9
cw27 <kimcw001227@gmail.com>
2023-04-10 14:30:45 +0900

    Ex04

---
+++ src/day01/Ex02.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		/*
+		 escape 문자 : 문자열 안에서 특수한 기능을 하는 것
+		 문자열 : 큰 따옴표로 묶여있는 값들
+		 \n : 줄바꿈
+		 \t : Tab(Spacebar 8칸만큼 오른쪽으로 이동)
+		 \\ : \ 하나 표현
+		 \" : " 하나 표현
+		 */
+		System.out.println("안\n녕\n하\n세\n요");
+		System.out.println("1\t1234567\t12345678\t1");
+		
+		System.out.println("이름\t나이");
+		System.out.println("홍길동\t20살");
+		
+		System.out.println("\\");
+		System.out.println("\"");
+	}
+
+}
---
+++ src/day01/Ex03.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		System.out.println(100);	//일반 숫자는 큰 따옴표 없이 숫자만 입력
+		System.out.println(100.123);
+		
+		System.out.println(100 + 100);	//일반 숫자는 큰 따옴표 없이 숫자만 입력
+		System.out.println(100.123 + 100.123);
+		
+		System.out.println("백 : " + 100);
+		System.out.println("100" + 100 + 100);
+		System.out.println("100" + (100 + 100));
+	}
+
+}
